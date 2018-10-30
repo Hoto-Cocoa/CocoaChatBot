bd845647c69cb6728126a7e734e445f3f0058c36
Hoto Cocoa <cocoa@hoto.us>
2018-10-30 20:03:51 +0900

    Add Vote Command

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const Config = require('./config');
 const Sauce = require('sagiri');
 const https = require('https');
 const math = require('mathjs');
-
+const Database = require('./modules/Database');
+const jsonQuery = require('json-query');
 
 const logger = Logger.createLogger({
 	format: Logger.format.combine(
@@ -25,12 +26,14 @@ const logger = Logger.createLogger({
 });
 const telegramBot = new TelegramBot(Config.Telegram.Token, { polling: true });
 const sauce = new Sauce(Config.SauceNAO.Token);
+const database = new Database(Config.Database, logger);
 
 google.resultsPerPage = 20;
 math.config({
 	number: 'BigNumber',
 	precision: 64
 });
+require('./modules/CreateDatabase')(Config.Database, logger);
 
 const tmpDir = require('os').tmpdir();
 
@@ -107,13 +110,132 @@ telegramBot.on('message', msg => {
 	if(msgText.toLowerCase() === 'info') {
 		return telegramBot.sendMessage(msg.chat.id, JSON.stringify(msg.reply_to_message ? msg.reply_to_message : msg), { reply_to_message_id: msg.message_id });
 	}
+
+	if(msgText.toLowerCase().startsWith('vote ')) {
+		var msgArr = msgText.substring(5, msgText.length).split(' ');
+		const action = msgArr.shift().toLowerCase();
+		var username = msg.from.username ? `@${msg.from.username}` : msg.from.last_name ? `${msg.from.first_name} ${msg.from.last_name}` : msg.from.first_name;
+		msgArr = msgArr.join('').split(',');
+
+		if(action === 'create') {
+			logger.log('notice', 'User %s Used Vote Command(Create %s) in %s(%s)', `${username}(${msg.from.id})`, msgArr.join(', '), msg.chat.title, msg.chat.id);
+			const name = msgArr.shift();
+			database.query('INSERT INTO vote(date, groupId, userId, username, name, data) VALUES(?, ?, ?, ?, ?, ?);', [
+				Date.now(), msg.chat.id, msg.from.id, username, name, JSON.stringify({
+					selections: msgArr
+				})
+			]).then(res => {
+				return telegramBot.sendMessage(msg.chat.id, `Created vote "${name}"(#${res.insertId}).`, { reply_to_message_id: msg.message_id });
+			});
+		}
+
+		if(action === 'view') {
+			const id = msgArr.shift();
+			logger.log('notice', 'User %s Used Vote Command(View %s) in %s(%s)', `${username}(${msg.from.id})`, id, msg.chat.title, msg.chat.id);
+			database.query('SELECT * FROM vote WHERE id=?;', id).then(res => {
+				if(!res[0].deleted) return telegramBot.sendMessage(msg.chat.id, 'This vote was deleted.', { reply_to_message_id: msg.message_id });
+				const data = JSON.parse(res[0].data);
+				var inlineBtnArr = [];
+				for(var i = 0; i < data.selections.length; i++) {
+					inlineBtnArr.push([{
+						text: data.selections[i],
+						callback_data: JSON.stringify({
+							action: 'VoteVoting',
+							vote: id,
+							value: i
+						})
+					}]);
+				}
+				return telegramBot.sendMessage(msg.chat.id, `<b>${res[0].name}</b>`, { parse_mode: 'HTML', reply_to_message_id: msg.message_id, reply_markup: { inline_keyboard: inlineBtnArr }});
+			});
+		}
+
+		if(action === 'open') {
+			const id = msgArr.shift();
+			logger.log('notice', 'User %s Used Vote Command(Open %s) in %s(%s)', `${username}(${msg.from.id})`, id, msg.chat.title, msg.chat.id);
+			database.query('SELECT groupId, name, closed, deleted FROM vote WHERE id=?;', id).then(res => {
+				if(parseInt(res[0].groupId) !== msg.chat.id) return telegramBot.sendMessage(msg.chat.id, 'This vote not created in this chat. Please execute this command in chat that this vote created.', { reply_to_message_id: msg.message_id });
+				if(!res[0].deleted) return telegramBot.sendMessage(msg.chat.id, 'This vote was deleted.', { reply_to_message_id: msg.message_id });
+				if(res[0].closed === 0) return telegramBot.sendMessage(msg.chat.id, 'This vote not closed.', { reply_to_message_id: msg.message_id });
+				database.query('UPDATE vote SET closed=1 WHERE id=?;', id).then(() => {
+					return telegramBot.sendMessage(msg.chat.id, `Vote ${res[0].name}(#${id}) opened.`, { reply_to_message_id: msg.message_id });
+				});
+			});
+		}
+
+		if(action === 'close') {
+			const id = msgArr.shift();
+			logger.log('notice', 'User %s Used Vote Command(Close %s) in %s(%s)', `${username}(${msg.from.id})`, id, msg.chat.title, msg.chat.id);
+			database.query('SELECT groupId, name, closed, delete FROM vote WHERE id=?;', id).then(res => {
+				if(parseInt(res[0].groupId) !== msg.chat.id) return telegramBot.sendMessage(msg.chat.id, 'This vote not created in this chat. Please execute this command in chat that this vote created.', { reply_to_message_id: msg.message_id });
+				if(!res[0].deleted) return telegramBot.sendMessage(msg.chat.id, 'This vote was deleted.', { reply_to_message_id: msg.message_id });
+				if(res[0].closed === 1) return telegramBot.sendMessage(msg.chat.id, 'This vote already closed.', { reply_to_message_id: msg.message_id });
+				database.query('UPDATE vote SET closed=1 WHERE id=?;', id).then(() => {
+					return telegramBot.sendMessage(msg.chat.id, `Vote ${res[0].name}(#${id}) closed.`, { reply_to_message_id: msg.message_id });
+				});
+			});
+		}
+
+		if(action === 'result') {
+			const id = msgArr.shift();
+			logger.log('notice', 'User %s Used Vote Command(Result %s) in %s(%s)', `${username}(${msg.from.id})`, id, msg.chat.title, msg.chat.id);
+			database.query('SELECT groupId, name, data, closed, deleted FROM vote WHERE id=?;', id).then(res => {
+				if(parseInt(res[0].groupId) !== msg.chat.id) return telegramBot.sendMessage(msg.chat.id, 'This vote not created in this chat. Please execute this command in chat that this vote created.', { reply_to_message_id: msg.message_id });
+				if(!res[0].deleted) return telegramBot.sendMessage(msg.chat.id, 'This vote was deleted.', { reply_to_message_id: msg.message_id });
+				var data = JSON.parse(res[0].data);
+				var name = res[0].name;
+				database.query('SELECT username, value FROM voting WHERE voteId=? AND active=1;', id).then(res => {
+					var selections = [];
+					for(var i = 0; i < data.selections.length; i++) {
+						var q = jsonQuery(`[**][*value=${i}].username`, { data: { data: res }}).value;
+						selections.push(`<b>${data.selections[i]}</b>: ${q.length}`);
+					}
+					return telegramBot.sendMessage(msg.chat.id, `<b>${name}</b>\n\n${selections.join('\n')}`, { parse_mode: 'HTML', reply_to_message_id: msg.message_id });
+				});
+			});
+		}
+
+		if(action === 'aresult') {
+			const id = msgArr.shift();
+			logger.log('notice', 'User %s Used Vote Command(AResult %s) in %s(%s)', `${username}(${msg.from.id})`, id, msg.chat.title, msg.chat.id);
+			database.query('SELECT groupId, name, data, closed, deleted FROM vote WHERE id=?;', id).then(res => {
+				if(parseInt(res[0].groupId) !== msg.chat.id) return telegramBot.sendMessage(msg.chat.id, 'This vote not created in this chat. Please execute this command in chat that this vote created.', { reply_to_message_id: msg.message_id });
+				if(!res[0].deleted) return telegramBot.sendMessage(msg.chat.id, 'This vote was deleted.', { reply_to_message_id: msg.message_id });
+				var data = JSON.parse(res[0].data);
+				var name = res[0].name;
+				database.query('SELECT username, value FROM voting WHERE voteId=? AND active=1;', id).then(res => {
+					var selections = [];
+					for(var i = 0; i < data.selections.length; i++) {
+						var q = jsonQuery(`[**][*value=${i}].username`, { data: { data: res }}).value;
+						selections.push(`<b>${data.selections[i]}</b>: ${q.length}${q.length ? `(${q.join(' ,')})` : ''}`);
+					}
+					return telegramBot.sendMessage(msg.chat.id, `<b>${name}</b>\n\n${selections.join('\n')}`, { parse_mode: 'HTML', reply_to_message_id: msg.message_id });
+				});
+			});
+		}
+
+		if(action === 'delete') {
+			const id = msgArr.shift();
+			logger.log('notice', 'User %s Used Vote Command(Delete %s) in %s(%s)', `${username}(${msg.from.id})`, id, msg.chat.title, msg.chat.id);
+			database.query('SELECT groupId, name, closed, deleted FROM vote WHERE id=?;', id).then(res => {
+				if(parseInt(res[0].groupId) !== msg.chat.id) return telegramBot.sendMessage(msg.chat.id, 'This vote not created in this chat. Please execute this command in chat that this vote created.', { reply_to_message_id: msg.message_id });
+				if(!res[0].closed) return telegramBot.sendMessage(msg.chat.id, 'This vote not closed. Please close vote first.', { reply_to_message_id: msg.message_id });
+				if(res[0].deleted) return telegramBot.sendMessage(msg.chat.id, 'This vote already deleted.', { reply_to_message_id: msg.message_id });
+				database.query('UPDATE vote SET deleted=1 WHERE id=?;', id).then(() => {
+					return telegramBot.sendMessage(msg.chat.id, `Vote ${res[0].name}(#${id}) deleted.`, { reply_to_message_id: msg.message_id });
+				});
+			});
+		}
+	}
 });
 
 telegramBot.on('callback_query', msg => {
 	const data = JSON.parse(msg.data);
+	const name = msg.from.username ? `@${msg.from.username}` : msg.from.last_name ? `${msg.from.first_name} ${msg.from.last_name}` : msg.from.first_name;
+
 	if(data.action === 'MathMoreNumber') {
 		const expression = msg.message.reply_to_message.text.substring(1, msg.message.reply_to_message.text.length);
-		logger.log('notice', 'User %s Used Math More Button(Calculate %s, More Number %s) in %s(%s)', `@${msg.message.from.username}(${msg.message.from.id})`, expression, data.value, msg.message.chat.title , msg.message.chat.id);
+		logger.log('notice', 'User %s Used Math More Button(Calculate %s, More Number %s) in %s(%s)', `${name}(${msg.message.from.id})`, expression, data.value, msg.message.chat.title , msg.message.chat.id);
 		https.get(`https://api.wolframalpha.com/v2/query?input=${encodeURIComponent(expression)}&primary=true&appid=${Config.Wolfram.Token}&podstate=${data.value}@Result__More+digits&podstate=${data.value}@DecimalApproximation__More+digits&format=plaintext&output=json&podtitle=Result&podtitle=Decimal%20approximation&podtitle=Power%20of%2010%20representation&podtitle=Exact%20result`, res => {
 			var json = '';
 			res.on('data', data => {
@@ -130,6 +252,30 @@ telegramBot.on('callback_query', msg => {
 					} ] ] }
 				}, options);
 				return telegramBot.editMessageText(value, options);
+			});
+		});
+	}
+
+	if(data.action === 'VoteVoting') {
+		logger.log('notice', 'User %s Used Vote Voting Button(Vote to %s, Value %s) in %s(%s)', `@${name}(${msg.message.from.id})`, data.vote, data.value, msg.message.chat.title , msg.message.chat.id);
+		database.query('SELECT closed FROM vote WHERE id=?', data.vote).then(res => {
+			if(res[0].closed) return telegramBot.answerCallbackQuery(msg.id, {
+				text: 'This vote was closed.'
+			});
+			database.query(`SELECT id FROM voting WHERE voteId=? AND userId=? ORDER BY id DESC LIMIT 1`, [
+				data.vote, msg.from.id
+			]).then(res => {
+				if(res.length) {
+					database.query(`UPDATE voting SET active=0 WHERE id=?`, res[0].id);
+				}
+				database.query('UPDATE vote SET count=count+1 WHERE id=?;', data.vote).then(() => {
+					database.query('INSERT INTO voting(date, voteId, userId, username, value) VALUES(?, ?, ?, ?, ?);', [
+						Date.now(), data.vote, msg.from.id, name, data.value
+					]);
+					return telegramBot.answerCallbackQuery(msg.id, {
+						text: 'Your voting was recorded.'
+					});
+				});
 			});
 		});
 	}
---
+++ modules/CreateDatabase.js
@@ -0,0 +1,99 @@
+const Sequelize = require('sequelize');
+
+module.exports = (config, logger) => {
+	const sequelize = new Sequelize(config.database, config.user, config.password, {
+		dialect: 'mysql',
+		pool: {
+			max: 30,
+			min: 0,
+			acquire: 30000,
+			idle: 10000
+		},
+		logging: sequelizeLogger
+	});
+	const queryInterface = sequelize.getQueryInterface();
+
+	queryInterface.createTable( 'vote', {
+		id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		date: {
+			type: Sequelize.BIGINT,
+			allowNull: false
+		},
+		count: { 
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			defaultValue: 0
+		},
+		groupId:  {
+			type: Sequelize.BIGINT,
+			allowNull: false
+		},
+		userId:  {
+			type: Sequelize.BIGINT,
+			allowNull: false
+		},
+		username:  {
+			type: Sequelize.TEXT,
+			allowNull: false
+		},
+		name:  {
+			type: Sequelize.TEXT,
+			allowNull: false
+		},
+		data: {
+			type: Sequelize.TEXT,
+			allowNull: false
+		},
+		closed: {
+			type: Sequelize.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		},
+		deleted: {
+			type: Sequelize.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	});
+	
+	queryInterface.createTable( 'voting', {
+		id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		date: {
+			type: Sequelize.BIGINT,
+			allowNull: false
+		},
+		voteId: { 
+			type: Sequelize.INTEGER,
+			allowNull: false
+		},
+		userId:  {
+			type: Sequelize.BIGINT,
+			allowNull: false
+		},
+		username:  {
+			type: Sequelize.TEXT,
+			allowNull: false
+		},
+		value: {
+			type: Sequelize.INTEGER,
+			allowNull: false
+		},
+		active: {
+			type: Sequelize.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		}
+	});
+	
+	function sequelizeLogger(msg) {
+		logger.log('debug', 'Sequelize Executed "%s"', msg);
+	}
+}
