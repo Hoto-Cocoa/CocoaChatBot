bcfccb1acf174d47d61e673363c27afb5638cf77
Hoto Cocoa <cocoa@hoto.us>
2018-12-20 23:24:25 +0900

    Improve Get Command

--- commands/get.js
+++ commands/get.js
@@ -6,7 +6,7 @@ module.exports = (bot, logger, utils) => {
 		const username = msg.from.username ? `@${msg.from.username}` : msg.from.last_name ? `${msg.from.first_name} ${msg.from.last_name}` : msg.from.first_name;
 		const language = utils.getLanguage(msg.from.language_code).get;
 
-		if(msgText.startsWith('get')) {
+		if(msgText.startsWith('get ')) {
 			const id = parseInt(msgText.substring(4));
 			const readableId = msg.from.language_code && msg.from.language_code.substring(0, 2) === 'en' ? id + (id === 1 ? 'st' : id === 2 ? 'nd' : id === 3 ? 'rd' : 'th') : id;
 			logger.log('notice', 'User %s Used Get Command(Get %s) in %s(%s)', `${username}(${msg.from.id})`, msgText.substring(4), msg.chat.title, msg.chat.id);
