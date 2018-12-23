db61626557d701a7bad1dc8e058dced485fa5ced
Hoto Cocoa <cocoa@hoto.us>
2018-12-23 12:53:24 +0900

    Improve Info Command

--- commands/info.js
+++ commands/info.js
@@ -10,18 +10,18 @@ module.exports = (bot, logger, utils) => {
 			var toSendArr = [];
 			toSendArr.push(`<b>${getLanguage('messageId')}</b>: ${msgData.message_id}`);
 			toSendArr.push(`<b>${getLanguage('date')}</b>: ${msgData.date}`);``
-			toSendArr.push();
+			toSendArr.push('');
 			toSendArr.push(`<b>${getLanguage('userId')}</b>: ${msgData.from.id}`);
 			toSendArr.push(`<b>${getLanguage('userIsBot')}</b>: ${msgData.from.is_bot}`);
 			toSendArr.push(`<b>${getLanguage('userFirstName')}</b>: ${msgData.from.first_name}`);
 			if(msgData.from.last_name) toSendArr.push(`<b>${getLanguage('userLastName')}</b>: ${msgData.from.last_name}`);
 			if(msgData.from.username) toSendArr.push(`<b>${getLanguage('userUsername')}</b>: ${msgData.from.username}`);
 			if(msgData.from.language_code) toSendArr.push(`<b>${getLanguage('userLanguageCode')}</b>: ${msgData.from.language_code}`);
-			toSendArr.push();
+			toSendArr.push('');
 			toSendArr.push(`<b>${getLanguage('chatId')}</b>: ${msgData.chat.id}`);
 			if(msgData.chat.title) toSendArr.push(`<b>${getLanguage('chatTitle')}</b>: ${msgData.chat.title}`);
 			toSendArr.push(`<b>${getLanguage('chatType')}</b>: ${msgData.chat.type}`);
-			toSendArr.push();
+			toSendArr.push('');
 			toSendArr.push(`<b>${getLanguage('debug')}</b>: ${JSON.stringify(msgData)}`);
 			return bot.sendMessage(msg.chat.id, toSendArr.join('\n'), { reply_to_message_id: msg.message_id, parse_mode: 'HTML' });
 		}
