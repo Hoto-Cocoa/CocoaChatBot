1287bae21f03a20933252257bd8d08465f8b741f
Hoto Cocoa <cocoa@hoto.us>
2018-12-24 23:52:21 +0900

    Improve Search Command

--- commands/search.js
+++ commands/search.js
@@ -14,7 +14,7 @@ module.exports = (bot, logger) => {
 				for(let i = 0; i < res.links.length; i++) {
 					if(toSendMsgs.length >= 3) break;
 					var link = res.links[i];
-					if(link.description && link.href) toSendMsgs.push(`<a href="${encodeURI(link.href)}">${link.title.replace('<', '&lt;').replace('>', '&gt;')}</a>\n${link.description.replace('<', '&lt;').replace('>', '&gt;').replace('\n', ' ')}`);
+					if(link.description && link.href) toSendMsgs.push(`<a href="${link.href}">${link.title.replace('<', '&lt;').replace('>', '&gt;')}</a>\n${link.description.replace('<', '&lt;').replace('>', '&gt;').replace('\n', ' ')}`);
 				}
 				return bot.sendMessage(msg.chat.id, toSendMsgs.join('\n\n'), { parse_mode: 'HTML', reply_to_message_id: msg.message_id });
 			});
