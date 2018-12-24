c9bc43b35d10e58a3c64db06e963eedf075b9942
Hoto Cocoa <cocoa@hoto.us>
2018-12-24 21:36:51 +0900

    Improve Language Class

--- modules/Language.js
+++ modules/Language.js
@@ -7,9 +7,9 @@ module.exports = class Language {
 	}
 
 	async getLanguage(langCode, userId, msgCategory) {
-		var languageSetting = await this.database.query('SELECT value FROM setting WHERE userId=? AND `key`="language" AND active=1;', userId);
-		var langCode = languageSetting ? languageSetting : langCode ? langCode.substring(0, 2) : 'en';
-		var languageData = this.languages[langCode] ? this.languages[langCode] : this.languages['en'];
+		var languageSetting = await this.database.query('SELECT value FROM setting WHERE userId=? AND `key`="language" AND active=1;', userId), languageData = null;
+		langCode = languageSetting ? languageSetting : langCode ? langCode.substring(0, 2) : 'en';
+		languageData = this.languages[langCode] ? this.languages[langCode] : this.languages['en'];
 		return (msgCode, ...args) => {
 			var msg = languageData[msgCategory] ? languageData[msgCategory][msgCode] ? languageData[msgCategory][msgCode] : this.languages['en'][msgCategory][msgCode] ? this.languages['en'][msgCategory][msgCode] : 'Error: Invalid Message Code' : this.languages['en'][msgCategory] ? this.languages['en'][msgCategory][msgCode] ? this.languages['en'][msgCategory][msgCode] : 'Error: Invalid Message Code' : 'Error: Invalid Message Code';
 			return args ? require('util').format(msg, ...args) : msg;
@@ -18,7 +18,7 @@ module.exports = class Language {
 
 	async getLanguageData(language, userId) {
 		var languageSetting = await this.database.query('SELECT value FROM setting WHERE userId=? AND `key`="language" AND active=1;', userId);
-		var language = languageSetting ? languageSetting : language ? language.substring(0, 2) : 'en';
+		language = languageSetting ? languageSetting : language ? language.substring(0, 2) : 'en';
 		return this.languages[language] ? this.languages[language] : this.languages['en'];
 	}
 }
