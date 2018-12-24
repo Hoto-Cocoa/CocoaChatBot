584732d9d441c7e4466ce704cb7e5d3adae484df
Hoto Cocoa <cocoa@hoto.us>
2018-12-24 22:30:53 +0900

    Improve Language Class

--- modules/Language.js
+++ modules/Language.js
@@ -7,9 +7,9 @@ module.exports = class Language {
 	}
 
 	async getLanguage(langCode, userId, msgCategory) {
-		var languageSetting = await this.database.query('SELECT value FROM setting WHERE userId=? AND `key`="language" AND active=1;', userId), languageData = null;
+		var languageSetting = await this.database.query('SELECT value FROM setting WHERE userId=? AND `key`="language" AND active=1;', userId);
 		langCode = languageSetting ? languageSetting : langCode ? langCode.substring(0, 2) : 'en';
-		languageData = this.languages[langCode] ? this.languages[langCode] : this.languages['en'];
+		var languageData = this.languages[langCode] ? this.languages[langCode] : this.languages['en'];
 		return (msgCode, ...args) => {
 			var msg = languageData[msgCategory] ? languageData[msgCategory][msgCode] ? languageData[msgCategory][msgCode] : this.languages['en'][msgCategory][msgCode] ? this.languages['en'][msgCategory][msgCode] : 'Error: Invalid Message Code' : this.languages['en'][msgCategory] ? this.languages['en'][msgCategory][msgCode] ? this.languages['en'][msgCategory][msgCode] : 'Error: Invalid Message Code' : 'Error: Invalid Message Code';
 			return args ? require('util').format(msg, ...args) : msg;
