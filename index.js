08f56f16e474be55967144f6376b6121df3e9f7b
Hoto Cocoa <cocoa@hoto.us>
2018-12-24 19:40:48 +0900

    Fix Callback Query

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ telegramBot.on('callback_query', async msg => {
 	if(data.action === 'SchoolChoice') {
 		logger.log('notice', 'User %s Used School Choice Button(School %s, Type %s) in %s(%s)', `${name}(${msg.message.from.id})`, data.code, data.type, msg.message.chat.title, msg.message.chat.id);
 		schoolMeal.get(data.type, data.code, (err, res) => {
-			if(err) return (logger.log('error', e.stack) && telegramBot.editMessageText('Error!', { chat_id: msg.message.chat.id, message_id: msg.message.message_id, reply_to_message_id: msg.message.reply_to_message.message_id });
+			if(err) return (logger.log('error', e.stack) && telegramBot.editMessageText('Error!', { chat_id: msg.message.chat.id, message_id: msg.message.message_id, reply_to_message_id: msg.message.reply_to_message.message_id }));
 			return telegramBot.editMessageText(res, { chat_id: msg.message.chat.id, message_id: msg.message.message_id, reply_to_message_id: msg.message.reply_to_message.message_id });
 		});
 	}
