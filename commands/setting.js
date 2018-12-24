f3218310dc6e6f33918e6c85436140343ef1f30f
Hoto Cocoa <cocoa@hoto.us>
2018-12-24 22:39:47 +0900

    Improve Setting Command

--- commands/setting.js
+++ commands/setting.js
@@ -2,7 +2,7 @@ module.exports = (bot, logger, utils) => {
 	bot.on('message', async msg => {
 		const msgText = msg.text ? msg.text : msg.caption ? msg.caption : '';
 		const username = msg.from.username ? `@${msg.from.username}` : msg.from.last_name ? `${msg.from.first_name} ${msg.from.last_name}` : msg.from.first_name;
-		const getLanguage = await utils.language.getLanguage(msg.from.language_code, msg.from.id, 'setting');
+		var getLanguage = await utils.language.getLanguage(msg.from.language_code, msg.from.id, 'setting');
 
 		if(msgText.startsWith('setting ')) {
 			const msgArr = msgText.substring(8).split(' ');
@@ -11,7 +11,10 @@ module.exports = (bot, logger, utils) => {
 				if(id) utils.database.query('UPDATE setting SET active=0 WHERE id=?', id);
 				utils.database.query('INSERT INTO setting(date, userId, `key`, value) VALUES(?, ?, ?, ?);', [
 					Date.now(), msg.from.id, msgArr[0], msgArr[1]
-				]).then(() => bot.sendMessage(msg.chat.id, getLanguage('done'), { reply_to_message_id: msg.message_id }));
+				]).then(async () => {
+					getLanguage = await utils.language.getLanguage(msg.from.language_code, msg.from.id, 'setting');
+					bot.sendMessage(msg.chat.id, getLanguage('done'), { reply_to_message_id: msg.message_id });
+				});
 			});
 		}
 	});
