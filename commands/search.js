b761b7a88a42c59ed9f39f7db46121671affc395
Hoto Cocoa <cocoa@hoto.us>
2019-01-02 22:56:43 +0900

    Add GoogleSearch Class, Improve Search Command

--- commands/search.js
+++ commands/search.js
@@ -1,22 +1,19 @@
-const google = require('google');
-google.resultsPerPage = 20;
+const GoogleSearch = require('../modules/GoogleSearch');
+const googleSearch = new GoogleSearch(20);
 
 module.exports = (telegram, logger, utils) => {
 	telegram.events.on('message', async msg => {
 		const getLanguage = await utils.language.getLanguage(msg.from.language_code, msg.from.id, 'search');
 		if(msg.text.startsWith('//')) {
 			logger.log('notice', 'User %s Used Search Command(Search %s) in %s(%s)', `${msg.from.parsed_username}(${msg.from.id})`, msg.text.substring(2), msg.chat.title, msg.chat.id);
-			google(msg.text.substring(2), function(err, res) {
-				if(err) logger.log('error', err);
-				var toSendMsgs = [];
-				for(let i = 0; i < res.links.length; i++) {
-					if(toSendMsgs.length >= 3) break;
-					var link = res.links[i];
-					if(link.description && link.href) toSendMsgs.push(`<a href="${link.href}">${link.title.replace('<', '&lt;').replace('>', '&gt;')}</a>\n${link.description.replace('<', '&lt;').replace('>', '&gt;').replace('\n', ' ')}`);
-				}
-				return telegram.bot.sendMessage(msg.chat.id, toSendMsgs.join('\n\n'), { parse_mode: 'HTML', reply_to_message_id: msg.message_id }).catch(() => {
-					return telegram.bot.sendMessage(msg.chat.id, getLanguage('noResult'), { reply_to_message_id: msg.message_id });
-				});
+			var toSendMsgs = [];
+			const result = await googleSearch.getResult(msg.text.substring(2));
+			for(var i = 0; i < result.length; i++) {
+				if(toSendMsgs.length >= 3) break;
+				if(result[i].description && result[i].href) toSendMsgs.push(`<a href="${result[i].href}">${result[i].title.replace('<', '&lt;').replace('>', '&gt;')}</a>\n${result[i].description.replace('<', '&lt;').replace('>', '&gt;').replace('\n', ' ')}`);
+			}
+			return telegram.bot.sendMessage(msg.chat.id, toSendMsgs.join('\n\n'), { parse_mode: 'HTML', reply_to_message_id: msg.message_id }).catch(() => {
+				return telegram.bot.sendMessage(msg.chat.id, getLanguage('noResult'), { reply_to_message_id: msg.message_id });
 			});
 		}
 	});
---
+++ modules/GoogleSearch.js
@@ -0,0 +1,25 @@
+const asyncRequest = require('./AsyncRequest');
+const cheerio = require('cheerio')
+const util = require('util');
+
+module.exports = class GoogleSearch {
+	constructor(maxResultCount = 10) {
+		this.maxResultCount = maxResultCount;
+	}
+
+	async getResult(searchValue = '') {
+		return new Promise(async (resolve) => {
+			const $ = cheerio.load(await asyncRequest(util.format('https://www.google.com/search?hl=en&q=%s&sa=N&num=%s&ie=UTF-8&oe=UTF-8&gws_rd=ssl', encodeURI(searchValue), this.maxResultCount)));
+			var result = [];
+			$('div.g').each((i, e) => {
+				$(e).find('h3.r a')[0] && result.push({
+					index: i,
+					title: $(e).find('h3.r a').first().text(),
+					href: $(e).find('h3.r a')[0].attribs.href.replace(/(?:\/url\?q=)?(.*?)(?:&sa=.*)/, '$1'),
+					description: $(e).find('div.s').find('span.st').text()
+				});
+			});
+			resolve(result);
+		});
+	}
+}
