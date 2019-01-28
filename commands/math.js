dbdabffeab302932cae504795b95ec6fcc2c24a0
Hoto Cocoa <cocoa@hoto.us>
2019-01-29 02:27:22 +0900

    Improve Math Command

--- commands/math.js
+++ commands/math.js
@@ -1,9 +1,5 @@
-const asyncRequest = require('../modules/AsyncRequest');
 const math = require('mathjs');
-math.config({
-	number: 'BigNumber',
-	precision: 64
-});
+math.config({ number: 'BigNumber', precision: 64 });
 
 module.exports = (telegram, logger, utils) => {
 	telegram.events.on('message', async msg => {
@@ -22,7 +18,7 @@ module.exports = (telegram, logger, utils) => {
 					throw new Error();
 				}
 			} catch(e) {
-				const result = JSON.parse((await asyncRequest(`https://api.wolframalpha.com/v2/query?input=${encodeURIComponent(input)}&primary=true&appid=${require('../config').Wolfram.Token}&format=plaintext&output=json&podtitle=Result&podtitle=Decimal%20approximation&podtitle=Power%20of%2010%20representation&podtitle=Exact%20result`)).body);
+				const result = JSON.parse((await require('../modules/AsyncRequest')(`https://api.wolframalpha.com/v2/query?input=${encodeURIComponent(input)}&primary=true&appid=${require('../config').Wolfram.Token}&format=plaintext&output=json&podtitle=Result&podtitle=Decimal%20approximation&podtitle=Power%20of%2010%20representation&podtitle=Exact%20result`)).body);
 				if(!result.queryresult.pods) return telegram.bot.sendMessage(msg.chat.id, getLanguage('wrongInput'), { reply_to_message_id: msg.message_id });
 				const value = result.queryresult.pods[0].subpods[0].plaintext;
 				var options = { reply_to_message_id: msg.message_id };
