6c0dfc88e07b36780a2576acb4fbd9d3069511e8
Hoto Cocoa <cocoa@hoto.us>
2019-01-29 02:27:31 +0900

    Improve Search Command

--- commands/search.js
+++ commands/search.js
@@ -1,13 +1,10 @@
-const GoogleSearch = require('../modules/GoogleSearch');
-const googleSearch = new GoogleSearch(20);
-
 module.exports = (telegram, logger, utils) => {
 	telegram.events.on('message', async msg => {
 		const getLanguage = await utils.language.getLanguage(msg.from.language_code, msg.from.id, 'search');
 		if(msg.command_text.startsWith('//')) {
 			logger.log('notice', 'User %s Used Search Command(Search %s) in %s(%s)', `${msg.from.parsed_username}(${msg.from.id})`, msg.command_text.substring(2), msg.chat.title, msg.chat.id);
 			var toSendMsgs = [];
-			const result = await googleSearch.getResult(msg.command_text.substring(2));
+			const result = await new (require('../modules/GoogleSearch'))(20).getResult(msg.command_text.substring(2));
 			for(var i = 0; i < result.length; i++) {
 				if(toSendMsgs.length >= 3) break;
 				if(result[i].description && result[i].href) toSendMsgs.push(`<a href="${result[i].href}">${result[i].title.replace('<', '&lt;').replace('>', '&gt;')}</a>\n${result[i].description.replace('<', '&lt;').replace('>', '&gt;').replace('\n', ' ')}`);
