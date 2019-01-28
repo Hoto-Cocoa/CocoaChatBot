c1ba699bbda90c69313d71e17e5a3fb777471a50
Hoto Cocoa <cocoa@hoto.us>
2019-01-29 02:26:11 +0900

    Improve SchoolChoice Callback

--- callbacks/SchoolChoice.js
+++ callbacks/SchoolChoice.js
@@ -1,10 +1,8 @@
-const schoolMeal = new (require('../modules/SchoolMeal'))();
-
 module.exports = (telegram, logger, utils) => {
 	telegram.events.on('callback_query', async msg => {
 		if(msg.data.action === 'SchoolChoice') {
 			logger.log('notice', 'User %s Used School Choice Button(School %s, Type %s) in %s(%s)', `${msg.from.parsed_username}(${msg.message.from.id})`, msg.data.code, msg.data.type, msg.message.chat.title, msg.message.chat.id);
-			const meal = await schoolMeal.getMeal(msg.data.type, msg.data.code).catch(e => {
+			const meal = await new (require('../modules/SchoolMeal'))().getMeal(msg.data.type, msg.data.code).catch(e => {
 				switch(e) {
 					case 'NOT_SUPPORTED': telegram.bot.editMessageText(`Not supported school! (Type: ${msg.data.code.substring(0, 1)})`, { chat_id: msg.message.chat.id, message_id: msg.message.message_id, reply_to_message_id: msg.message.reply_to_message.message_id }); break;
 					case 'NO_DATA': telegram.bot.editMessageText('No data!', { chat_id: msg.message.chat.id, message_id: msg.message.message_id, reply_to_message_id: msg.message.reply_to_message.message_id }); break;
