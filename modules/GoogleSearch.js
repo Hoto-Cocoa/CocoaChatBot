0da6bf94dfe69adfd2d71ef4eac043f1e738b3aa
Hoto Cocoa <cocoa@hoto.us>
2020-12-07 21:23:09 +0900

    Update GoogleSearch Module

--- modules/GoogleSearch.js
+++ modules/GoogleSearch.js
@@ -20,12 +20,12 @@ module.exports = class GoogleSearch {
 			const { body } = await require('./AsyncRequest')(`https://www.google.com/search?hl=en&q=${encodeURI(searchValue)}&sa=N&num=${this.maxResultCount}&ie=UTF-8&oe=UTF-8&gws_rd=ssl`, { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36' });
 			const $ = require('cheerio').load(body);
 			var result = [];
-			$('div.rc').each((i, e) => {
-				$(e).find('div.s').first().text() && result.push({
+			$('div.g').each((i, e) => {
+				$(e).find('h3.LC20lb').first().text() && result.push({
 					index: i,
 					title: $(e).find('h3.LC20lb').first().text(),
-					href: $(e).find('div.r a').first().attr('href'),
-					description: $(e).find('div.s').first().text()
+					href: $(e).find('div.yuRUbf a').first().attr('href'),
+					description: $(e).find('span.aCOpRe').first().text()
 				});
 			}); 
 			resolve(result);
